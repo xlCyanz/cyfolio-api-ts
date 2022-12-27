9a78fc27cc7a7bcadef3a37154e3a1bfc1fe81b6
Johan E. Sierra Linares <johanse.linares@gmail.com>
2022-12-27 14:51:48 -0400

    Update server.ts

--- config/env/production/server.ts
+++
@@ -1,9 +0,0 @@
-export default ({ env }) => ({
-  proxy: true,
-  host: env('HOST', '0.0.0.0'),
-  url: env("PUBLIC_URL", "http://localhost:1337"),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
-});
-
